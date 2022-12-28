d5a0adab067641df278084b8a5f5add94014a9c7
13archit <chopra13archit@gmail.com>
2022-12-28 17:39:33 +0530

    Initial Commit

---
+++ _example/exmaple.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source  = "clouddrove/resource-group/azure"
+  version = "1.0.0"
+
+  name        = "app-13"
+  environment = "test"
+  label_order = ["environment", "name", ]
+  location    = "North Europe"
+}
+
+module "vnet" {
+  source = "../"
+
+  name                = "app"
+  environment         = "test"
+  label_order         = ["name", "environment"]
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.0.0.0/16"
+  enable_ddos_pp      = false
+}
