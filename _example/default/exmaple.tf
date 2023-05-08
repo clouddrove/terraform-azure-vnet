0e666ab90316a099bb9fae27237e716c5a3b0b33
Mahesh Yadav <85216933+themaheshyadav@users.noreply.github.com>
2023-05-08 08:32:42 +0530

    bug: update source version of modules (#11)
    * bug: update source version of modules
    
    * bug: fix fmt issue

--- _example/default/exmaple.tf
+++ _example/default/exmaple.tf
@@ -17,7 +17,6 @@ module "vnet" {
 
   name                = "app"
   environment         = "test"
-  label_order         = ["name", "environment"]
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   address_space       = "10.0.0.0/16"
