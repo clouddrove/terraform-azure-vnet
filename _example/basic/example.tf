b784738a4dfb12be32a58e7bdbd4522eeb48abf8
Archit Chopra <chopra13archit@gmail.com>
2023-06-15 17:51:15 +0530

    fix: fixed statis checks error

--- _example/basic/example.tf
+++ _example/basic/example.tf
@@ -11,8 +11,8 @@ module "vnet" {
   source                 = "../../"
   name                   = local.name
   environment            = local.environment
-  resource_group_name    = module.resource_group.resource_group_name
-  location               = module.resource_group.resource_group_location
+  resource_group_name    = "app-test"
+  location               = "NorthEurope"
   address_space          = "10.0.0.0/16"
   enable_network_watcher = false # To be set true when network security group flow logs are to be tracked and network watcher with specific name is to be deployed. 
 }
