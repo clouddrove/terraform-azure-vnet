f49103bb63b9654adc3e1cfe20166f85398c04d7
mamrajyadav <themamraj0131@gmail.com>
2023-12-12 17:56:01 +0530

    feat: add github/workflows

--- _example/complete/example.tf
+++ _example/complete/example.tf
@@ -29,5 +29,6 @@ module "vnet" {
   resource_group_name    = module.resource_group.resource_group_name
   location               = module.resource_group.resource_group_location
   address_space          = "10.0.0.0/16"
+  enable_ddos_pp         = false
   enable_network_watcher = false # To be set true when network security group flow logs are to be tracked and network watcher with specific name is to be deployed.
 }
--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "vnet_id" {
-  description = "The id of the newly created vNet"
   value       = azurerm_virtual_network.vnet[*].id
+  description = "The id of the newly created vNet"
 }
 
 output "vnet_name" {
-  description = "The name of the newly created vNet"
   value       = azurerm_virtual_network.vnet[*].name
+  description = "The name of the newly created vNet"
 }
 
 output "vnet_location" {
-  description = "The location of the newly created vNet"
   value       = azurerm_virtual_network.vnet[*].location
+  description = "The location of the newly created vNet"
 }
 
 output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
   value       = azurerm_virtual_network.vnet[*].address_space
+  description = "The address space of the newly created vNet"
 }
 
 output "vnet_guid" {
-  description = "The GUID of the virtual network."
   value       = azurerm_virtual_network.vnet[*].guid
+  description = "The GUID of the virtual network."
 }
 
 output "vnet_rg_name" {
-  description = "The name of the resource group in which to create the virtual network. Changing this forces a new resource to be created"
   value       = azurerm_virtual_network.vnet[*].resource_group_name
+  description = "The name of the resource group in which to create the virtual network. Changing this forces a new resource to be created"
 }
 
 output "ddos_protection_plan_id" {
