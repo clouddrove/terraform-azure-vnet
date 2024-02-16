79f33ea99c02585d48542ca5203e96612d1b531d
Rupal Sharma <152851038+Rupalgw@users.noreply.github.com>
2024-02-16 14:19:29 +0530

    fix: change in count condition (#31)
    * fix: change in count condition in outputs.
    
    * fix: change in count condition with null in outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,49 @@
 output "vnet_id" {
-  value       = azurerm_virtual_network.vnet[0].id
+  value       = try(azurerm_virtual_network.vnet[0].id, null)
   description = "The id of the newly created vNet"
 }
 
 output "vnet_name" {
-  value       = azurerm_virtual_network.vnet[0].name
+  value       = try(azurerm_virtual_network.vnet[0].name, null)
   description = "The name of the newly created vNet"
 }
 
 output "vnet_location" {
-  value       = azurerm_virtual_network.vnet[0].location
+  value       = try(azurerm_virtual_network.vnet[0].location, null)
   description = "The location of the newly created vNet"
 }
 
 output "vnet_address_space" {
-  value       = azurerm_virtual_network.vnet[0].address_space
+  value       = try(azurerm_virtual_network.vnet[0].address_space, null)
   description = "The address space of the newly created vNet"
 }
 
 output "vnet_guid" {
-  value       = azurerm_virtual_network.vnet[0].guid
+  value       = try(azurerm_virtual_network.vnet[0].guid, null)
   description = "The GUID of the virtual network."
 }
 
 output "vnet_rg_name" {
-  value       = azurerm_virtual_network.vnet[0].resource_group_name
+  value       = try(azurerm_virtual_network.vnet[0].resource_group_name, null)
   description = "The name of the resource group in which to create the virtual network. Changing this forces a new resource to be created"
 }
 
 output "ddos_protection_plan_id" {
-  value       = var.enable_ddos_pp && var.enable ? azurerm_network_ddos_protection_plan.example[0].id : null
+  value       = try(var.enable_ddos_pp && var.enable ? azurerm_network_ddos_protection_plan.example[0].id : null, null)
   description = "The ID of the DDoS Protection Plan"
 }
 
 output "network_watcher_id" {
-  value       = var.enable && var.enable_network_watcher ? azurerm_network_watcher.flow_log_nw[0].id : null
+  value       = try(var.enable && var.enable_network_watcher ? azurerm_network_watcher.flow_log_nw[0].id : null, null)
   description = "The ID of the Network Watcher."
 }
 
 output "network_watcher_name" {
-  value       = var.enable && var.enable_network_watcher ? azurerm_network_watcher.flow_log_nw[0].name : null
+  value       = try(var.enable && var.enable_network_watcher ? azurerm_network_watcher.flow_log_nw[0].name : null, null)
   description = "The name of Network Watcher deployed."
 }
 
 output "ddos_existing_plan_id" {
-  value       = azurerm_virtual_network.vnet[0].ddos_protection_plan
+  value       = try(azurerm_virtual_network.vnet[0].ddos_protection_plan, null)
   description = "The ID of the DDoS Protection Plan"
 }
